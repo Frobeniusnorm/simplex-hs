8715dde598c17895bb37dc56d833006f044c2d17
Frobeniusnorm <david.schwarzbeck@fau.de>
2023-07-07 17:08:58 +0200

    spelling

--- Simplex1.hs
+++ Simplex1.hs
@@ -1,16 +1,19 @@
+{-# HLINT ignore "Redundant lambda" #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module Simplex1 where
 
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant lambda" #-}
-import Numeric.LinearAlgebra
-import Debug.Trace (trace)
-import Prelude hiding ((<>))
-import Data.List (minimumBy)
 import Data.Function (on)
-{-# LANGUAGE ScopedTypeVariables #-}
+import Data.List (minimumBy)
+import Debug.Trace (trace)
+import Numeric.LinearAlgebra
+import Prelude hiding ((<>))
+
 simplex :: Matrix R -> Vector R -> Vector R -> Maybe (Vector R)
-{- | Implementation of the formal Simplex algorithm
-Takes a maximization problem in natural form -}
+
+-- | Implementation of the formal Simplex algorithm
+-- Takes a maximization problem in natural form
 simplex a b c = do
   -- highly inefficient !
   -- choose two last elements
@@ -22,26 +25,26 @@ simplex a b c = do
     simplexIt a x b c active = do
       -- calculate directions
       let e = \i -> tr (ident (fst $ size a) ? [i])
-      let ws = [linearSolveLS (a ? active) (- e i ? active) | i <- active]      -- which improve the direction ? 
+      let ws = [linearSolveLS (a ? active) (-e i ? active) | i <- active] -- which improve the direction ?
       let costs = [(c <.> flatten w, i) | (w, i) <- ws `zip` [0 .. length ws]] :: [(R, Int)]
       let imprv = filter (\w -> fst w > 0) costs
       -- check if there are improvements, if not -> already optimal
-      if null imprv then
-        Just x
-      else do
-        -- choose j and w as the first improvement cost
-        let (_, wi) = head imprv
-        let w = ws !! wi
-        let j = active !! wi
-        -- calculate inactive indices
-        let inactive = filter (`notElem` active) [0 .. fst (size a) - 1]
-        -- calculate lowest magnitude we can go in direction w
-        let gammas = [(((b ! i) - (((a ? [i]) #> x) ! 0)) / flatten ((a ? [i]) <> w) ! 0, i) | i <- inactive]
-        let validg = filter (\g -> fst g > 0) gammas
-        -- check if there are valid gammas (gammas > 0), if not -> unbounded
-        if null validg then
-          Nothing
+      if null imprv
+        then Just x
         else do
-          let (gamma, k) = minimumBy (compare `on` fst) validg
-          -- move along w with amount gamma, j leaves the basis, k enters it
-          simplexIt a (x + (flatten w * realToFrac gamma)) b c (k:filter (/= j) active)
+          -- choose j and w as the first improvement cost
+          let (_, wi) = head imprv
+          let w = ws !! wi
+          let j = active !! wi
+          -- calculate inactive indices
+          let inactive = filter (`notElem` active) [0 .. fst (size a) - 1]
+          -- calculate lowest magnitude we can go in direction w
+          let gammas = [(((b ! i) - (((a ? [i]) #> x) ! 0)) / flatten ((a ? [i]) <> w) ! 0, i) | i <- inactive]
+          let validg = filter (\g -> fst g > 0) gammas
+          -- check if there are valid gammas (gammas > 0), if not -> unbounded
+          if null validg
+            then Nothing
+            else do
+              let (gamma, k) = minimumBy (compare `on` fst) validg
+              -- move along w with amount gamma, j leaves the basis, k enters it
+              simplexIt a (x + (flatten w * realToFrac gamma)) b c (k : filter (/= j) active)
